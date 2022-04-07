3865c2b1122cf1e4278619407f0b458e881e3d52
juangil991 <jcgo125@gmail.com>
2022-04-06 22:18:14 -0500

    se crea el data y el reducer de los phones mas el combine reducers

---
+++ src/reducers/phones.jsx
@@ -0,0 +1,11 @@
+import { RECEIVE_PHONES } from '../actions/phones'export default function phones(state={}, action){
+    switch(action.type){
+    case RECEIVE_PHONES:
+    return {
+    ...state,
+    ...action.phones
+    }
+    default:
+    return state
+    }
+    }
