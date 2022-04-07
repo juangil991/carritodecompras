4eea5030d4a4a6390a86d6c474c73ffe58779f1f
juangil991 <jcgo125@gmail.com>
2022-04-06 22:31:57 -0500

    se crea el store y se añade le provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+let store = createStore(reducer)
+render(
+<Provider store={ store }>
+<App />
+</Provider>,
+document.getElementById('root')
 );
 
--- src/reducers/phones.js
+++ src/reducers/phones.js
@@ -1,4 +1,6 @@
-import { RECEIVE_PHONES } from '../actions/phones'export default function phones(state={}, action){
+import { RECEIVE_PHONES } from '../actions/phones'
+
+export default function phones(state={}, action){
     switch(action.type){
     case RECEIVE_PHONES:
     return {
